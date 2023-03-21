7b730657e896777912cfd5a2d6c2c287f9ffcfcd
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 16:27:38 +0530

    Add : Calculating Employee Wages using Switch case

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -1,38 +1,34 @@
 package com.bridgelabz.employeewagecomputationproblems;
 
 public class EmployeeWageComputationProblems {
+	public static final int IS_PART_TIME =1;
+	public static final int IS_FULL_TIME =2;
+	public static final int EMP_RATE_PER_HOUR =20;
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problems");
 		
-		 int IS_PART_TIME = 1;
-		 int IS_FULL_TIME = 2;
-		 int EMP_RATE_PER_HOUR = 20;
+		
 		 int empHrs = 0;
 		 int empWage = 0;
 		 
 		 
-		    double empCheck = Math.floor(Math.random() * 10) % 3;
-		    if (empCheck == IS_PART_TIME) {
-		    	empHrs =4;
-		    	empWage = empHrs * EMP_RATE_PER_HOUR;
-		    	System.out.println("Employee Wage :" + empWage );
-		  } else if (empCheck == IS_FULL_TIME ) {
-		        empHrs =8;
-		        empWage = empHrs * EMP_RATE_PER_HOUR;
-		    	System.out.println("Employee Wage :" + empWage );
-		        
-		  } else {
-			  empHrs =0;
-			  empWage = empHrs * EMP_RATE_PER_HOUR;
-		    	System.out.println("Employee Wage :" + empWage );
-			  
+		    int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		    switch (empCheck) {
+		    case IS_PART_TIME:
+		    	empHrs =4 ;
+		    	break;
+		    case IS_FULL_TIME:
+		    	empHrs = 8;
+		    	break;
+		    case 3 :
+		    	empHrs = 0;
+		    
 		  }
-		    	
-		    	
-		    	
-		    }
+		    empWage = empHrs * EMP_RATE_PER_HOUR;
+		    System.out.println("Employee Wages :" + empWage);
 		    	
 		    
-
+	}
 }
