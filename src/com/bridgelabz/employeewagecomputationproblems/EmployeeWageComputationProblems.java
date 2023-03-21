4709baf814a79477cad476c7ede968b01cbc2cd2
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 15:52:20 +0530

    Add : Problem to Check the Employee wages per day

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -5,13 +5,23 @@ public class EmployeeWageComputationProblems {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problems");
 		 int IS_FULL_TIME = 1;
+		 int EMP_RATE_PER_HOUR = 20;
+		 int empHrs = 0;
+		 int empWage = 0;
 		    double empCheck = Math.floor(Math.random() * 10) % 2;
 		    if (empCheck == IS_FULL_TIME) {
+		    	empHrs =8;
 		    	System.out.println("The Employee Is Present");
+		    	empWage=empHrs*EMP_RATE_PER_HOUR;
+		    	System.out.println("Employee Wage is :" + empWage);
 		    	
 		    }else {
+		    	empHrs=0;
 		    	System.out.println("The Employee Is Absent");
+		    	empWage=empHrs*EMP_RATE_PER_HOUR;
+		    	System.out.println("Employee Wage is :" + empWage);
 		    }
+		    
 
 	}
 
