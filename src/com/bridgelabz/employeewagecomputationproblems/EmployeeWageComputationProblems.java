4ecbd9419de65f7770e1855ff6de6ce431aa0310
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 15:43:13 +0530

    Add : Problem to Check whether the employee is Present or Absent

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -4,6 +4,14 @@ public class EmployeeWageComputationProblems {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problems");
+		 int IS_FULL_TIME = 1;
+		    double empCheck = Math.floor(Math.random() * 10) % 2;
+		    if (empCheck == IS_FULL_TIME) {
+		    	System.out.println("The Employee Is Present");
+		    	
+		    }else {
+		    	System.out.println("The Employee Is Absent");
+		    }
 
 	}
 
