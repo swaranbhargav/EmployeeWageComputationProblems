0487541840a2afd3276e5f6f51bcbd1fbcd3d7cf
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 16:52:19 +0530

    Add : Monthly wages of the employees

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -4,7 +4,7 @@ public class EmployeeWageComputationProblems {
 	public static final int IS_PART_TIME =1;
 	public static final int IS_FULL_TIME =2;
 	public static final int EMP_RATE_PER_HOUR =20;
-	
+	public static final int NUM_OF_WORKING_DAYS =20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problems");
@@ -12,9 +12,9 @@ public class EmployeeWageComputationProblems {
 		
 		 int empHrs = 0;
 		 int empWage = 0;
-		 
-		 
-		    int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		 int totalEmpWages = 0;
+		 for (int day = 0; day <= NUM_OF_WORKING_DAYS; day++) {
+			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		    switch (empCheck) {
 		    case IS_PART_TIME:
 		    	empHrs =4 ;
@@ -27,8 +27,11 @@ public class EmployeeWageComputationProblems {
 		    
 		  }
 		    empWage = empHrs * EMP_RATE_PER_HOUR;
+		    totalEmpWages = totalEmpWages + empWage;
 		    System.out.println("Employee Wages :" + empWage);
 		    	
 		    
+		 }
+		 System.out.println("Total Employee Wages is :" + totalEmpWages);
 	}
 }
