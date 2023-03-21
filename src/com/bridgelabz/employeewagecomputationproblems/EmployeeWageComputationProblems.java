278533e74c4ad5c5946efb251a9e9fbcccd6d1fc
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 16:13:14 +0530

    Add : checking the wages of the part time employee

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -4,25 +4,35 @@ public class EmployeeWageComputationProblems {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problems");
-		 int IS_FULL_TIME = 1;
+		
+		 int IS_PART_TIME = 1;
+		 int IS_FULL_TIME = 2;
 		 int EMP_RATE_PER_HOUR = 20;
 		 int empHrs = 0;
 		 int empWage = 0;
-		    double empCheck = Math.floor(Math.random() * 10) % 2;
-		    if (empCheck == IS_FULL_TIME) {
-		    	empHrs =8;
-		    	System.out.println("The Employee Is Present");
-		    	empWage=empHrs*EMP_RATE_PER_HOUR;
-		    	System.out.println("Employee Wage is :" + empWage);
+		 
+		 
+		    double empCheck = Math.floor(Math.random() * 10) % 3;
+		    if (empCheck == IS_PART_TIME) {
+		    	empHrs =4;
+		    	empWage = empHrs * EMP_RATE_PER_HOUR;
+		    	System.out.println("Employee Wage :" + empWage );
+		  } else if (empCheck == IS_FULL_TIME ) {
+		        empHrs =8;
+		        empWage = empHrs * EMP_RATE_PER_HOUR;
+		    	System.out.println("Employee Wage :" + empWage );
+		        
+		  } else {
+			  empHrs =0;
+			  empWage = empHrs * EMP_RATE_PER_HOUR;
+		    	System.out.println("Employee Wage :" + empWage );
+			  
+		  }
 		    	
-		    }else {
-		    	empHrs=0;
-		    	System.out.println("The Employee Is Absent");
-		    	empWage=empHrs*EMP_RATE_PER_HOUR;
-		    	System.out.println("Employee Wage is :" + empWage);
+		    	
+		    	
 		    }
+		    	
 		    
-
-	}
 
 }
