dfc8b78cd41e32843e837f637c8472352cf30477
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 17:09:07 +0530

    Add : we have caliculated the total number of days and hours the employee work and also caliculated their wages

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -5,15 +5,17 @@ public class EmployeeWageComputationProblems {
 	public static final int IS_FULL_TIME =2;
 	public static final int EMP_RATE_PER_HOUR =20;
 	public static final int NUM_OF_WORKING_DAYS =20;
+	public static final int MAX_HRS_IN_MONTH =160;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problems");
 		
 		
 		 int empHrs = 0;
-		 int empWage = 0;
-		 int totalEmpWages = 0;
-		 for (int day = 0; day <= NUM_OF_WORKING_DAYS; day++) {
+		 int totalEmpHrs = 0;
+		 int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
+			totalWorkingDays++;
 			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		    switch (empCheck) {
 		    case IS_PART_TIME:
@@ -26,12 +28,12 @@ public class EmployeeWageComputationProblems {
 		    	empHrs = 0;
 		    
 		  }
-		    empWage = empHrs * EMP_RATE_PER_HOUR;
-		    totalEmpWages = totalEmpWages + empWage;
-		    System.out.println("Employee Wages :" + empWage);
+		    totalEmpHrs = totalEmpHrs + empHrs;
+		  
+		    System.out.println("Total Days :" + totalWorkingDays + "employee Hrs" + empHrs);
 		    	
-		    
 		 }
+		int totalEmpWages  = totalEmpHrs * EMP_RATE_PER_HOUR;
 		 System.out.println("Total Employee Wages is :" + totalEmpWages);
 	}
 }
