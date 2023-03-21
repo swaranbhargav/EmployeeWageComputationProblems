527da9ca458d0127cbe3d43576f53869d69a8c59
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-21 15:39:19 +0530

    Add : Welcome to Employee Wage Computation Problems

---
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewagecomputationproblems;
+
+public class EmployeeWageComputationProblems {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Problems");
+
+	}
+
+}
