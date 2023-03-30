7f4a49c83360d2b0cd569c91438b907972c40bcd
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-30 16:56:38 +0530

    Add: Class method and Class Variable to compute employee wages

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -1,39 +1,39 @@
 package com.bridgelabz.employeewagecomputationproblems;
 
+import java.util.Random;
+
 public class EmployeeWageComputationProblems {
-	public static final int IS_PART_TIME =1;
-	public static final int IS_FULL_TIME =2;
-	public static final int EMP_RATE_PER_HOUR =20;
-	public static final int NUM_OF_WORKING_DAYS =20;
-	public static final int MAX_HRS_IN_MONTH =160;
+
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int MAX_HRS_IN_MONTHS = 100;
+
+	public static int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
+	public static int numOfWorkingDays = 20;
+
+	private static void wageComputation() {
+		Random random = new Random();
+		while (totalEmpHrs < MAX_HRS_IN_MONTHS && totalWorkingDays < numOfWorkingDays) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+			}
+			totalEmpHrs = totalEmpHrs + empHrs;
+		}
+
+		int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage is : " + empWage);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Problems");
-		
-		
-		 int empHrs = 0;
-		 int totalEmpHrs = 0;
-		 int totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
-			totalWorkingDays++;
-			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-		    switch (empCheck) {
-		    case IS_PART_TIME:
-		    	empHrs =4 ;
-		    	break;
-		    case IS_FULL_TIME:
-		    	empHrs = 8;
-		    	break;
-		    case 3 :
-		    	empHrs = 0;
-		    
-		  }
-		    totalEmpHrs = totalEmpHrs + empHrs;
-		  
-		    System.out.println("Total Days :" + totalWorkingDays + "employee Hrs" + empHrs);
-		    	
-		 }
-		int totalEmpWages  = totalEmpHrs * EMP_RATE_PER_HOUR;
-		 System.out.println("Total Employee Wages is :" + totalEmpWages);
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		wageComputation();
 	}
 }
