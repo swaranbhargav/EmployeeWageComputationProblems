b70ad9c502290e5a559b819bc535065d5ed84731
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-04-07 17:00:56 +0530

    Add: Refactor to use arraylist in place of array

--- src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
+++ src/com/bridgelabz/employeewagecomputationproblems/EmployeeWageComputationProblems.java
@@ -2,59 +2,29 @@ package com.bridgelabz.employeewagecomputationproblems;
 
 import java.util.*;
 
-interface InterfaceComputeEmpWage {
-	public void addCompanyEmpWage(String company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth);
+public class EmployeeWageComputationProblems {
 
-	public void computeEmpWage();
-}
-
-class CompanyEmpWage {
-
-	public final String company;
-	public final int empRatePerHours;
-	public final int numOfWorkingDays;
-	public final int maxHoursPerMonth;
-	public int totalEmpWage;
-
-	public CompanyEmpWage(String company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth) {
-		this.company = company;
-		this.empRatePerHours = empRatePerHours;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursPerMonth = maxHoursPerMonth;
-	}
-
-	public void setTotalEmpWage(int totalEmpWage) {
-		this.totalEmpWage = totalEmpWage;
-	}
-
-	@Override
-	public String toString() {
-		return "Total Employee Wage for Company:" + company + " is: " + totalEmpWage + "\n";
-	}
-}
-
-public class EmployeeWageComputationProblems implements InterfaceComputeEmpWage {
-	// Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
 	private int numOfCompany = 0;
-	private CompanyEmpWage[] companyEmpWageArray;
+	private ArrayList<CompanyEmpWage> companyEmpWageArrayList;
 
 	public EmployeeWageComputationProblems() {
-		companyEmpWageArray = new CompanyEmpWage[5];
+		companyEmpWageArrayList = new ArrayList<>();
 	}
 
 	public void addCompanyEmpWage(String company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth) {
-		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHours, numOfWorkingDays,
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHours, numOfWorkingDays,
 				maxHoursPerMonth);
-		numOfCompany++;
+		companyEmpWageArrayList.add(companyEmpWage);
 	}
 
 	public void computeEmpWage() {
-		for (int i = 0; i < numOfCompany; i++) {
-			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-			System.out.println(companyEmpWageArray[i]);
+		for (int i = 0; i < companyEmpWageArrayList.size(); i++) {
+			CompanyEmpWage companyEmpWage = companyEmpWageArrayList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);
 		}
 	}
 
@@ -83,9 +53,8 @@ public class EmployeeWageComputationProblems implements InterfaceComputeEmpWage 
 
 	public static void main(String[] args) {
 		EmployeeWageComputationProblems empWageBuilder = new EmployeeWageComputationProblems();
-		empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 40);
-		empWageBuilder.addCompanyEmpWage("Walmart", 20, 30, 50);
+		empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 100);
+		empWageBuilder.addCompanyEmpWage("Walmart", 20, 30, 100);
 		empWageBuilder.computeEmpWage();
 	}
-
 }
